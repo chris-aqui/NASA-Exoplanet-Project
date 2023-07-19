2ac355dc65977932f3484df2bbdfadb8c4c9d9e3
Christine <2739361+christine-aqui@users.noreply.github.com>
2023-07-19 08:13:58 -0400

    update - added comments, updated csv, added expected output to readme

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 To run project type `node index.js` in the terminal
 
-Small app to find list of habitable Planets from Kepler KOI Table 
+Small app to find list of habitable Planets from Kepler KOI Table
+
+Console OutPut:
+
+```
+[
+  'Kepler-1652 b',
+  'Kepler-1410 b',
+  'Kepler-296 f',
+  'Kepler-442 b',
+  'Kepler-296 e',
+  'Kepler-1649 b',
+  'Kepler-62 f',
+  'Kepler-452 b'
+]
+8 habitable planets found!
+```
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
+// This function defines a list of habitable planets.
 const habitablePlanets = [];
 
+// This function determines whether a planet is habitable.
 function isHabitablePlanet(planet) {
+  // The planet is habitable if it is confirmed, has an insolation between 0.36 and 1.11, and has a planetary radius less than 1.6.
   return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
 }
 
+// This function reads the CSV file and parses it into an array of objects.
 fs.createReadStream('kepler_data.csv')
+  // This line tells the parser to ignore lines that start with a '#'.
   .pipe(parse({
     comment: '#',
     columns: true,
   }))
+  // This line iterates over the parsed data and adds any habitable planets to the list of habitable planets.
   .on('data', (data) => {
     if (isHabitablePlanet(data)) {
       habitablePlanets.push(data);
     }
   })
+  // This line prints an error message if there is an error.
   .on('error', (err) => {
     console.log(err);
   })
+  // This line prints the names of the habitable planets and the number of habitable planets found.
   .on('end', () => {
     console.log(habitablePlanets.map((planet) => {
       return planet['kepler_name'];
